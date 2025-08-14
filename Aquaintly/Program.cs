08d7ffe43d6712c19b8350f8e5e92b193b3627f8
Raja Vasanth R <rajavasanth1@gmail.com>
2025-08-13 20:22:05 -0700

    Add project files.

---
+++ Aquaintly/Program.cs
@@ -0,0 +1,126 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace Aquaintly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var events = new List<string[]>
+            {
+                new[] { "CONNECT","Alice", "Bob" },
+                new[] { "DISCONNECT","Bob", "Alice" },
+                new[] { "CONNECT","Alice", "Charlie" },
+                new[] { "CONNECT","Dennis", "Bob" },
+                new[] { "CONNECT","Pam", "Dennis" },
+                new[] { "DISCONNECT","Pam", "Dennis" },
+                new[] { "CONNECT","Pam", "Dennis" },
+                new[] { "CONNECT","Edward", "Bob" },
+                new[] { "CONNECT","Dennis", "Charlie" },
+                new[] { "CONNECT","Alice", "Nicole" },
+                new[] { "CONNECT","Pam", "Edward" },
+                new[] { "DISCONNECT","Dennis", "Charlie" },
+                new[] { "CONNECT","Dennis", "Edward" },
+                new[] { "CONNECT","Charlie", "Bob" },
+            };
+
+            var result= Grouping(events,3);
+            Console.WriteLine(String.Join(",",result.Item1));
+            Console.WriteLine(String.Join(",", result.Item2));
+        }
+
+
+        public static (List<string>,List<string>) Grouping(List<string[]> events, int count)
+        {
+            Dictionary<string, HashSet<string>> connections = new Dictionary<string, HashSet<string>>();
+
+            foreach (var e in events)
+            {
+                string action = e[0];
+                string user1 = e[1];
+                string user2 = e[2];
+
+                if(action=="CONNECT")
+                {
+                    if (!connections.ContainsKey(user1))
+                        connections.Add(user1, new HashSet<string>());
+                         
+                    if (!connections.ContainsKey(user2))
+                        connections.Add(user2, new HashSet<string>());
+               
+                    connections[user2].Add(user1);
+                    connections[user1].Add(user2);
+                }
+                else
+                {
+                    if(connections.ContainsKey(user1))
+                        connections[user1].Remove(user2);
+
+                    if (connections.ContainsKey(user2))
+                        connections[user2].Remove(user1);
+                }
+            }
+
+            List<string> lessthan=new List<string>();
+            List<string> greaterthan=new List<string>();
+
+            foreach(var connection in connections)
+            {
+                if (connection.Value.Count < count)
+                    lessthan.Add(connection.Key);
+                else
+                    greaterthan.Add(connection.Key);
+            }
+
+            return (lessthan, greaterthan);
+        }
+
+        //public static (List<string>,List<string>) Grouping(List<string[]> events, int count)
+        /////public static void Grouping(List<string[]> events, int count)
+        //{
+        //    var connections=new Dictionary<string, HashSet<string>>();
+
+        //    foreach(var e in events)
+        //    {
+        //        var connectiontype = e[0];
+        //        var user1 = e[1];
+        //        var user2 = e[2];
+
+        //        if (connectiontype == "CONNECT")
+        //        {
+        //            if (!connections.ContainsKey(user1))
+        //                connections.Add(user1, new HashSet<string>());
+
+        //            if (!connections.ContainsKey(user2))
+        //                connections.Add(user2, new HashSet<string>());
+
+        //            connections[user1].Add(user2);
+        //            connections[user2].Add(user1);
+        //        }
+        //        else
+        //        {
+        //            if (connections.ContainsKey(user1))
+        //                connections.Remove(user2);
+
+        //            if (connections.ContainsKey(user2))
+        //                connections.Remove(user1);
+        //        }
+        //    }
+        //    List<string> lessthan = new List<string>();
+        //    List<string> greaterthan = new List<string>();
+
+        //    foreach(var connection in connections)
+        //    {
+        //        if (connection.Value.Count >= count)
+        //            greaterthan.Add(connection.Key);
+        //        else
+        //            lessthan.Add(connection.Key);
+        //    }
+
+        //    return (lessthan,greaterthan);
+        //}
+
+
+    }
+}
---
+++ BadgedAccess/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Reflection.Metadata;
+
+namespace BadgedAccess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var records1 = new List<string[]>
+          {
+              new[] {"Paul","enter"},
+              new[] {"Pauline","exit"},
+              new[] {"Paul","enter"},
+              new[] {"Paul","exit"},
+              new[] {"Martha","exit"},
+              new[] {"Joe","enter"},
+              new[] {"Martha","enter"},
+              new[] {"Steve","enter"},
+              new[] {"Martha","exit" },
+              new[] {"Jennifer","enter"},
+              new[] {"Joe","enter"},
+              new[] {"Curtis","exit" },
+              new[] {"Curtis", "enter"},
+              new[] {"Joe","exit"},
+              new[] { "Martha", "enter"},
+              new[] { "Martha", "exit"},
+              new[] { "Jennifer", "exit"},
+              new[] { "Joe", "enter"},
+              new[] { "Joe", "enter"},
+              new[] { "Martha", "exit"},
+              new[] { "Joe", "exit"},
+              new[] { "Joe", "exit"},
+          };
+
+            var records2 = new List<string[]>
+            {
+                new[] {"Paul","enter"},
+                new[] {"Paul","exit"}
+            };
+
+            var records3 = new List<string[]>
+            {
+                new[] {"Paul","enter"},
+                new[] {"Paul","enter"},
+                new[] {"Paul","exit"},
+                new[] {"Paul","exit"}
+            };
+
+            var records4 = new List<string[]>
+            {
+                new[] {"Raj","enter"},
+                new[] {"Paul","enter"},
+                new[] {"Paul","exit"},
+                new[] {"Paul","exit"},
+                new[] {"Paul","enter"},
+                new[] {"Raj","enter"},
+            };
+
+            Printresults(records1);
+            Printresults(records2);
+            Printresults(records3);
+            Printresults(records4);
+        }
+
+        public static void Printresults(List<string[]> records)
+        {
+            List<string> notexited = new List<string>();
+            List<string> notentered = new List<string>();
+            
+
+            Dictionary<string,List<string>> userentry = new Dictionary<string,List<string>>();
+
+            foreach (var record in records)
+            {
+                var user = record[0];
+                var entryorexit= record[1];
+
+                //if (entryorexit == "exit" && !notentered.Contains(user))
+                //    notentered.Add(user);
+
+                //if (entryorexit == "enter" && !notentered.Contains(user))
+                //    notexited.Add(user);
+                if (!userentry.ContainsKey(user))
+                    userentry.Add(user, new List<string>());
+
+                userentry[user].Add(entryorexit);
+                     
+            }
+
+            foreach(var user in userentry)
+            {
+                if(user.Value.Contains("enter"))
+                {
+                    var entercount = user.Value.Where(x => x == "enter").Count();
+                    var exitcount = user.Value.Where(x => x == "exit").Count();
+
+                    if(entercount > exitcount)
+                        notexited.Add(user.Key);
+                    else if (exitcount > entercount) 
+                        notentered.Add(user.Key);
+                }
+
+            }
+
+            Console.WriteLine(string.Join(", ", notexited));
+            Console.WriteLine(string.Join(", ", notentered));
+        }
+    }
+}
